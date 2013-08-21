2dda01f27e32c2c27ce54dc23042ac6e006ee56b
bibudha <bibudha.sahoo@gmail.com>
2013-08-21 11:48:14 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -125,76 +125,89 @@ var DescriptionVoice,EmailVoice,subjectVoice,mobileBackgroundVoice; // variables
     }
 	
 	 //Pdf Tab
-    function pdfTab()
-    {
-	//alert("coming");    
-	var featureRelId = getUrlVars()['transferId'];
-	var userSiteId = getUrlVars()['touchId'];
-	var featureId = getUrlVars()['mId'];
-	var url = baseUrl + 'web/web/getMenuHtml/' + featureId + '/' + featureRelId + '/' + userSiteId;
-	var data ='';	
-	//alert(url);
-		doAjaxCall(url, data, false, function (html) {
-	if ($.isEmptyObject(html)) {
-		$('#main-content').html('Sorry we have an Empty data');
-	} else {
-		if(html.length==1){
-		$.each(html, function (i,item){  
+   function pdfTab()
+      {
+  	//alert("coming");    
+  	var featureRelId = getUrlVars()['transferId'];
+  	var userSiteId = getUrlVars()['touchId'];
+  	var featureId = getUrlVars()['mId'];
+  	var url = baseUrl + 'web/web/getMenuHtml/' + featureId + '/' + featureRelId + '/' + userSiteId;
+  	var data ='';	
+  	var htmlData='';
+  	console.log(url);
+  		doAjaxCall(url, data, false, function (html) {
+  	if ($.isEmptyObject(html)) {
+  		$('#main-content').html('Sorry we have an Empty data');
+  	} else {
+  		//alert("empty");
+  		if(html.length==1){
+  		htmlData = '<ul data-role="listview" data-inset="false" data-divider-theme="d" id="aboutclass">';
+  		$.each(html, function (i,item){  
+  			//alert("empty");
+  			// onclick="window.open('+item.urlofFile+', '_blank', 'location=yes');"
+		 
+  			$.each(html,function(i,item){
+  				//var pdf_url=;
+  			htmlData += '<li><a href="pdf_d.html?itemUrl='+ item.urlOfFile +'&itemName=' + item.name + '" rel="external" >' + item.name + '</a></li>';
+  			});
+  			htmlData +='</ul>';	
+  			//alert(htmlData);
+  		})
 		
-		})
-		}else{
-			//pdf_d.html?itemId='+ item.wuFooId + '&transferId ='+ featureRelId + 'mId='+ featureId +'&touchId=' + userSiteId + '
-			// onclick="window.open('+item.urlofFile+', '_blank', 'location=yes');"
-		var htmlData = '<ul data-role="listview" data-inset="false" data-divider-theme="d" id="aboutclass">'
-			$.each(html,function(i,item){
-				//var pdf_url=;
-			htmlData += '<li><a href="pdf_d.html?itemUrl='+ item.urlOfFile +'&itemName=' + item.name + '" rel="external" >' + item.name + '</a></li>';
-			});
-			htmlData +='</ul>';	
-		}
+  		}else{
+  			//pdf_d.html?itemId='+ item.wuFooId + '&transferId ='+ featureRelId + 'mId='+ featureId +'&touchId=' + userSiteId + '
+  			// onclick="window.open('+item.urlofFile+', '_blank', 'location=yes');"
+  		 htmlData = '<ul data-role="listview" data-inset="false" data-divider-theme="d" id="aboutclass">'
+  			$.each(html,function(i,item){
+  				//var pdf_url=;
+  			htmlData += '<li><a href="pdf_d.html?itemUrl='+ item.urlOfFile +'&itemName=' + item.name + '" rel="external" >' + item.name + '</a></li>';
+  			});
+  			htmlData +='</ul>';	
+  			//alert(htmlData);
+  		}
 		
-		$('#main-content').html(htmlData);	
-		 try {
-      $("#aboutclass").listview('refresh');
-		} catch (e) {
-			$("#aboutclass").listview();
-		}
-	}
-	}); 
-    }
-    function pdfById()
-    {
+  		$('#main-content').html(htmlData);	
+  		 try {
+        $("#aboutclass").listview('refresh');
+  		} catch (e) {
+  			$("#aboutclass").listview();
+  		}
+  	}
+  	}); 
+      }
+      function pdfById()
+      {
 	  
-	var pdfUrl = getUrlVars()['itemUrl'];
-	var pdfTitle= getUrlVars()['itemName'];
-	//alert(pdfUrl);
-	//var url = baseUrl + 'web/web/getwufooById/' + wufooId + '/' + userSiteId;
-	//var data ='';	
-	var nav_url="http://docs.google.com/viewer?url="+ pdfUrl ;
-	//alert(nav_url);
-	$("#header_title").html(pdfTitle);
-	window.open(nav_url, '_self','location=yes');
-	//window.location.href=nav_url;
-	//navigator.load.url(pdfUrl);
-	//var ref = window.open(pdfUrl, '_blank');
-	//alert(ref);
-	//pdfActive(pdfUrl,returnUrl);
-	//alert(url);
-		/*doAjaxCall(url, data, false, function (html) {
-	if ($.isEmptyObject(html)) {
-		$('#main-content').html('Sorry we have an Empty data');
-	} else {
-		console.log(html);
-		$.each(html, function (i,item){ 
-		returnUrl = "pdf.html?mId=" + featureId + "&transferId=" + featureRelId + "&touchId=" + userSiteId;
-		 pdfActive(item.src,returnUrl);
-		})
+  	var pdfUrl = getUrlVars()['itemUrl'];
+  	var pdfTitle= getUrlVars()['itemName'];
+  	//alert(pdfUrl);
+  	//var url = baseUrl + 'web/web/getwufooById/' + wufooId + '/' + userSiteId;
+  	//var data ='';	
+  	var nav_url="http://docs.google.com/viewer?url="+ pdfUrl ;
+  	//alert(nav_url);
+  	$("#header_title").html(pdfTitle);
+  	window.open(nav_url, '_self','location=yes');
+  	//window.location.href=nav_url;
+  	//navigator.load.url(pdfUrl);
+  	//var ref = window.open(pdfUrl, '_blank');
+  	//alert(ref);
+  	//pdfActive(pdfUrl,returnUrl);
+  	//alert(url);
+  		/*doAjaxCall(url, data, false, function (html) {
+  	if ($.isEmptyObject(html)) {
+  		$('#main-content').html('Sorry we have an Empty data');
+  	} else {
+  		console.log(html);
+  		$.each(html, function (i,item){ 
+  		returnUrl = "pdf.html?mId=" + featureId + "&transferId=" + featureRelId + "&touchId=" + userSiteId;
+  		 pdfActive(item.src,returnUrl);
+  		})
 		
-		//$('#main-content').html(htmlData);	
-		}
-	}); */
+  		//$('#main-content').html(htmlData);	
+  		}
+  	}); */
 	
-    }
+      }
 	
 	/// <reference path="script.js" />
 
